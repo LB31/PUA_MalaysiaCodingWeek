0e65e5715518fc954ee99b93375a879dc9e500e3
Laura Amaro <Laura.MagdalenoAmaro@Student.HTW-Berlin.de>
2023-03-07 19:12:02 +0100

    Extension smt with plants
    good night:)

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,38 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ColorChanger : MonoBehaviour
 {
+    [HideInInspector] public bool islandIsActive = true;
 
+    [SerializeField] private Transform cameraTrans;
+    [SerializeField] private float radius = 1;
+    [SerializeField] private float secondsUntilShowingUI = 1;
+    [SerializeField] private LayerMask plantLayer;
+    [SerializeField] private List<PlantData> plantsData = new();
+
+    [Header("old")]
     [SerializeField] private MeshRenderer mesh;
-    [SerializeField] private List<ColorButton> colorButtons = new();
+
+    float duration = 0;
+    PlantData activePlant;
 
     private void Start()
     {
-        foreach (ColorButton button in colorButtons)
-            button.Button.onClick.AddListener(() => ChangeColor(button.Color));
+        foreach (PlantData data in plantsData)
+            data.Button.onClick.AddListener(() => mesh.material.color = data.Color);
     }
 
-    private void ChangeColor(Color col)
+    private void FixedUpdate()
     {
-        mesh.material.color = col;
+        if (Physics.SphereCast(cameraTrans.position, radius, cameraTrans.forward, out RaycastHit hit, 10, plantLayer))
+        {
+            duration += Time.fixedDeltaTime;
+
+            if (duration >= secondsUntilShowingUI)
+            {
+                Debug.Log(duration);
+                activePlant = plantsData.FirstOrDefault(x => x.plantUi.transform.parent.transform == hit.transform);
+                Debug.Log(activePlant);
+                ActivatePlantUI(true);
+            }
+
+            return;
+        }
+
+        duration = 0;
+        ActivatePlantUI(false);
+    }
+    private void ActivatePlantUI(bool activate)
+    {
+        activePlant.plantUi.SetActive(activate);
     }
 
     private void OnDestroy()
     {
-        foreach (ColorButton button in colorButtons)
+        foreach (PlantData data in plantsData)
         {
-            if (button.Button)
-                button.Button.onClick.RemoveAllListeners();
+            if (data.Button)
+                data.Button.onClick.RemoveAllListeners();
         }
     }
 }
 [Serializable]
-public struct ColorButton
+public struct PlantData
 {
     public Color Color;
     public Button Button;
+    public GameObject plantUi;
 }
