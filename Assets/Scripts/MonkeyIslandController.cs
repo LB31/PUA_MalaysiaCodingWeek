376ff1113700ac404afb89472b6c118a3cbcd334
Leonid Barsht <leonidb93@gmail.com>
2023-03-10 05:05:18 +0800

    fix many monkey bugs

--- Assets/Scripts/MonkeyIslandController.cs
+++ Assets/Scripts/MonkeyIslandController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MonkeyIslandController : MonoBehaviour
 {
+    public float Speed = 2;
+
     public void ScaleObject(Transform objToScale, float finalScale, float duration = 2)
     {
         StartCoroutine(ScaleCoroutine(objToScale, finalScale, duration));
@@ -24,7 +26,29 @@ public class MonkeyIslandController : MonoBehaviour
             objToScale.localScale = Vector3.Lerp(originalScale, Vector3.one * finalScale, t);
 
             yield return null;  // wait for the next frame
+        }
+    }
 
+    public void MoveObject(GameObject obj, Vector3 toLocalPos, float duration = 2)
+    {
+        StartCoroutine(MoveLocalCoroutine(obj, toLocalPos));
+    }
+
+    private IEnumerator MoveLocalCoroutine(GameObject obj, Vector3 toLocalPos, float duration = 2)
+    {
+        Vector3 originPos = obj.transform.position;
+        float currentTime = 0;
+
+        while (currentTime < duration)   // while still scaling
+        {
+            currentTime += Time.deltaTime;  // increment the current time
+
+            float t = currentTime / duration;  // calculate the current progress (0-1)
+
+            // set the object's new scale based on the current progress
+            obj.transform.localPosition = Vector3.Lerp(originPos, toLocalPos, t);
+
+            yield return null;  // wait for the next frame
         }
     }
 }
