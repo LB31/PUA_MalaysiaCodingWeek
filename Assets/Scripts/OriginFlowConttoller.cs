46a67a76d0b088150aafd27457844497702aff94
Laura Amaro <Laura.MagdalenoAmaro@Student.HTW-Berlin.de>
2023-03-09 21:28:07 +0100

    3d models origin island

--- Assets/Scripts/OriginFlowConttoller.cs
+++ Assets/Scripts/OriginFlowConttoller.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OriginFlowConttoller : MonoBehaviour
 {
     public GameObject[] content;
+    public GameObject panel;
     public Button nextButton;
+    public TextMeshProUGUI buttonText;
     private int actualStep = -1;
+
+    private readonly string nextText = "Next";
+    private readonly string restartText = "Restart";
 
     private void OnEnable()
     {
@@ -20,15 +26,28 @@ public class OriginFlowConttoller : MonoBehaviour
     }
     private void ActivateContent()
     {
+        //Panel
+        if (!panel.activeSelf)
+            panel.SetActive(true);
+
+        //Avoid errors on the first step
         if (actualStep != -1)
+        {
+            buttonText.text = nextText;
             content[actualStep].SetActive(false);
+        }
 
         actualStep++;
 
+        //Activate next content
         content[actualStep].SetActive(true);
 
         if (actualStep + 1 >= content.Length)
-            nextButton.gameObject.SetActive(false);
+            Restart();
     }
-
+    private void Restart()
+    {
+        actualStep = -1;
+        buttonText.text = restartText;
+    }
 }
